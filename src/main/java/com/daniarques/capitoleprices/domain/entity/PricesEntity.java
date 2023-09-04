0380ebbc7475b6f0883d101036f5bdf5df961bb0
daniarques <dani.arques11@gmail.com>
2023-09-04 18:47:47 +0200

    fix: Add composite PK

--- src/main/java/com/daniarques/capitoleprices/domain/entity/PricesEntity.java
+++ src/main/java/com/daniarques/capitoleprices/domain/entity/PricesEntity.java
@@ -1,35 +1,24 @@
 package com.daniarques.capitoleprices.domain.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Digits;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
-@Entity
+@Entity(name = "PRICES")
 public class PricesEntity {
 
-	@Column(name = "BRAND_ID")
-	@Digits(integer = 8, fraction = 0)
-	Integer brandId;
-
-	@Column(name = "START_DATE")
-	LocalDateTime startDate;
-
-	@Column(name = "END_DATE")
-	LocalDateTime endDate;
+	@EmbeddedId
+	PricesIdEntity id;
 
 	@Column(name = "PRICE_LIST")
 	@Digits(integer = 8, fraction = 0)
 	Integer priceList;
-
-	@Column(name = "PRODUCT_ID")
-	@Digits(integer = 8, fraction = 0)
-	Integer productId;
 
 	@Column(name = "PRIORITY")
 	@Digits(integer = 8, fraction = 0)
---
+++ src/main/java/com/daniarques/capitoleprices/domain/entity/PricesIdEntity.java
@@ -0,0 +1,27 @@
+package com.daniarques.capitoleprices.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Digits;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class PricesIdEntity {
+
+	@Column(name = "BRAND_ID")
+	@Digits(integer = 8, fraction = 0)
+	Integer brandId;
+
+	@Column(name = "START_DATE")
+	LocalDateTime startDate;
+
+	@Column(name = "END_DATE")
+	LocalDateTime endDate;
+
+	@Column(name = "PRODUCT_ID")
+	@Digits(integer = 8, fraction = 0)
+	Integer productId;
+
+}
